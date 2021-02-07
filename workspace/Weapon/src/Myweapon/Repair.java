9ae0727b7326678736c56399f10d6cdf88f60040
IceDice <dice7912@naver.com>
2021-02-07 19:28:35 +0900

    Repair Commit #2

--- workspace/Weapon/src/Myweapon/Repair.java
+++ workspace/Weapon/src/Myweapon/Repair.java
@@ -1,5 +1,22 @@
 package Myweapon;
 
 public interface Repair {
-
+	public int MAX_Durability=100;
+	public int MIN_Durability=0;
+	
+	public void Dorepair();
+	public void Donotrepair();
+	public void Setdurability(int durability);
+	
+	default void setwrap(boolean wrap) {
+		if(wrap) {
+			System.out.println("밀봉 처리합니다.");
+		} else {
+			System.out.println("밀봉 해제합니다.");
+		}
+	}
+	
+	static void changeWeapon() {
+		System.out.println("무기를 교체합니다.");
+	}
 }
